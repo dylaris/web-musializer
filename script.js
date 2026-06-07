c133827814498cfe33dd46eeed056c15b8ef5d29
Dylaris <3445818679@qq.com>
2026-06-07 17:12:36 +0800

    set the init height of control panel to 45%

--- script.js
+++ script.js
@@ -17,7 +17,7 @@
 
   // Panel size persistence
   let leftWidth = parseInt(localStorage.getItem('pake-left-width')) || 280;
-  let panelHeight = parseInt(localStorage.getItem('pake-panel-height')) || 33;
+  let panelHeight = parseInt(localStorage.getItem('pake-panel-height')) || 45;
   const leftMin = 180, leftMax = Math.floor(window.innerWidth * 0.5);
   const panelMin = 20, panelMax = 66;
 
